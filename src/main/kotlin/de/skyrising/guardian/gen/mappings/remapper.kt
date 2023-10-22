4a05c570fd449a6c764216055d65e5d36867be97
Fallen_Breath <yqx791125833@gmail.com>
2023-10-22 22:12:38 +0800

    the LocalVariableRenamer

--- src/main/kotlin/de/skyrising/guardian/gen/mappings/remapper.kt
+++ src/main/kotlin/de/skyrising/guardian/gen/mappings/remapper.kt
@@ -5,11 +5,14 @@ import org.objectweb.asm.*
 import org.objectweb.asm.commons.ClassRemapper
 import org.objectweb.asm.commons.Remapper
 import org.objectweb.asm.tree.ClassNode
+import org.objectweb.asm.tree.LocalVariableNode
 import org.objectweb.asm.tree.MethodInsnNode
 import org.objectweb.asm.tree.MethodNode
 import java.nio.file.Files
 import java.nio.file.Path
 import java.util.concurrent.CompletableFuture
+import java.util.regex.Pattern
+import javax.lang.model.SourceVersion
 
 class AsmRemapper(private val tree: MappingTree, private val superClasses: Map<String, Set<String>>, private val namespace: Int = tree.namespaces.size - 1) : Remapper() {
     override fun map(internalName: String?): String? {
@@ -121,6 +124,7 @@ fun mapJar(version: String, input: Path, output: Path, mappings: MappingTree, na
                     }
                     classNode.accept(classRemapper)
                     fixBridgeMethods(remappedNode)
+                    renameLocalVariables(remappedNode)
                     val remappedName = remapper.map(className) ?: throw IllegalArgumentException("$className could not be remapped")
                     if (remappedNode.sourceFile == null) {
                         var end = remappedName.indexOf('$')
@@ -137,6 +141,155 @@ fun mapJar(version: String, input: Path, output: Path, mappings: MappingTree, na
                 }
             }
         }
+    }
+}
+
+class LocalVariableRenamer {
+    private val names = HashSet<String>()
+    private val nameCounts = HashMap<String, Int>()
+    private val defaultLvNamePattern = Pattern.compile("^lvt\\d+$")
+
+    fun process(methodNode: MethodNode) {
+        if (methodNode.localVariables == null) {
+            return
+        }
+
+        val toRename = mutableListOf<LocalVariableNode>()
+        val isStatic = methodNode.access and Opcodes.ACC_STATIC != 0
+        for (lv in methodNode.localVariables) {
+            if (!isStatic && lv.index == 0) {
+                continue  // this
+            }
+            if (defaultLvNamePattern.matcher(lv.name).matches()) {
+                toRename.add(lv)
+            } else {
+                names.add(lv.name)
+                nameCounts[lv.name] = 1
+            }
+        }
+
+        for (lv in toRename) {
+            lv.name = generateNameFor(lv.desc, lv.name)
+        }
+    }
+
+    data class VarName(val varName: String, val incrementLetter: Boolean)
+
+    private fun generateVarName(desc: String): VarName? {
+        val type = desc.substringAfterLast('[')
+        var incrementLetter = true
+        return when (type[0]) {
+            'B' -> "b"
+            'C' -> "c"
+            'D' -> "d"
+            'F' -> "f"
+            'I' -> "i"
+            'J' -> "l"
+            'S' -> "s"
+            'Z' -> {
+                incrementLetter = false
+                "bl"
+            }
+
+            'L' -> run branch@{
+                incrementLetter = false
+                var className = type.substring(minOf(0, type.length))  // remove the 'L' prefix
+                className = className.substringAfterLast('/')
+                className = className.substringAfterLast('$')
+                className = className.substring(0, maxOf(className.length - 1, 0))  // remove the ';' suffix
+
+                if (className.isEmpty()) {
+                    output("lv-rename", "bad className, desc=$desc, className=$className")
+                    return@branch null
+                }
+
+                val varName = className[0].lowercaseChar() + className.substring(1)
+                if (varName != className && isValidJavaIdentifier(varName)) {
+                    varName
+                } else {
+                    // TODO: FIX "bad varName, desc=Lnet/minecraft/world/level/GameRules$1;, className=1, varName=1"
+                    output("lv-rename", "bad varName, desc=$desc, className=$className, varName=$varName")
+                    null
+                }
+            }
+            else -> {
+                output("lv-rename", "bad type, type=$type")
+                null
+            }
+        }?.let {
+            VarName(it, incrementLetter)
+        }
+    }
+
+    private fun generateNameFor(desc: String, fallback: String): String {
+        val varNameRet = generateVarName(desc) ?: return fallback
+        var varName = varNameRet.varName
+        val pluralVarName = varName + 's'
+        val incrementLetter = varNameRet.incrementLetter && varName.length == 1
+
+        val plural = desc[0] == '[' && !isJavaKeyword(pluralVarName)
+        if (plural) {
+            varName = pluralVarName
+        }
+
+        if (incrementLetter) {
+            var index = varName[0] - 'a'
+            while (names.contains(varName) || isJavaKeyword(varName)) {
+                varName = getIndexName(++index, plural)
+            }
+        } else {
+            var baseVarName = varName
+            if (isJavaKeyword(baseVarName)) {
+                baseVarName += '_'
+            }
+
+            var count: Int = nameCounts.compute(baseVarName) { _, v -> if (v == null) 1 else v + 1 }!!
+            while (true) {
+                varName = if (count == 1) {
+                    baseVarName
+                } else {
+                    baseVarName + count
+                }
+
+                if (!names.contains(varName)) {
+                    break
+                }
+                count++
+            }
+
+            nameCounts[baseVarName] = count
+        }
+
+        names.add(varName)
+        return varName
+    }
+
+    private fun getIndexName(index: Int, plural: Boolean): String {
+        val sb = StringBuilder(2)
+        var value = index
+        while (value > 0) {
+            sb.append('a' + value % 26)
+            value /= 26
+        }
+        var name = sb.toString()
+        if (plural) {
+            name += 's'
+        }
+        return name
+    }
+
+    private fun isValidJavaIdentifier(s: String): Boolean {
+        return s.isNotEmpty() && SourceVersion.isIdentifier(s) && s.codePoints().noneMatch { Character.isIdentifierIgnorable(it) }
+    }
+
+    private fun isJavaKeyword(s: String): Boolean {
+        return SourceVersion.isKeyword(s)
+    }
+}
+
+fun renameLocalVariables(remappedNode: ClassNode) {
+    for (methodNode in remappedNode.methods) {
+        LocalVariableRenamer().process(methodNode)
     }
 }
 
